906af4886fb759a20d0e6959ce62c08f02bd344a
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-05-02 15:49:27 +0300

    Initialise react-query

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { QueryClientProvider, QueryClient } from "react-query"
 import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components";
+import {Home} from "./routes/Home"
 
 function App() {
+
+	let queryClient = new QueryClient()
+
 	return (
-		<div style={{backgroundColor: "#0B192E", minHeight: "100vh"}}>
+		<QueryClientProvider client={queryClient} >
+			<div style={{backgroundColor: "#0B192E", minHeight: "100vh"}}>
 			<BrowserRouter>
 				<Navbar />
+				<Home />
 			</BrowserRouter>
 		</div>
+		</QueryClientProvider>
+		
 	);
 }
 
