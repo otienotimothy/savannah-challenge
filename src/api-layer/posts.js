d3b206fd050c84ffda28a45e93615566461ab31e
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-05-03 00:18:46 +0300

    Modify url to get a user's posts

--- src/api-layer/posts.js
+++ src/api-layer/posts.js
@@ -2,7 +2,7 @@ import { api } from "./api"
 
 export async function getUserPosts(userId){
     let result = await api.get(
-			`https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+			`https://jsonplaceholder.typicode.com/users/${userId}/posts`
 		);
 
     return result
